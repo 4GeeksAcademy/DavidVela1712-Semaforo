636de195a1616d95c3bf6d84e61dfb0fc3867906
DavidVela1712 <75803006+DavidVela1712@users.noreply.github.com>
2025-07-09 19:33:07 +0000

    Creaada forma del semáforo

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Container } from "react-bootstrap";
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { ImageLink } from "./components/ImageLink";
+
 
 export const App = () => {
   return (
     <>
-      <div>
-        <ImageLink src={viteLogo} alt={"Vite logo"} href={"https://vite.dev"} />
-        <ImageLink
-          src={reactLogo}
-          alt={"React logo"}
-          href={"https://react.dev"}
-        />
-      </div>
-      <h1>Vite + React</h1>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container fluid className="semaforo-contenedor">
+        <div className="semaforo-caja">
+          <div className="rojo"></div>
+          <div className="amarillo selected"></div>
+          <div className="verde"></div>
+        </div>
+      </Container>
     </>
   );
 };
