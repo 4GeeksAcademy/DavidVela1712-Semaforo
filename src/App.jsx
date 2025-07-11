0ba5951e0eeee49223f9b686ea5e20ff60eacc8f
DavidVela1712 <75803006+DavidVela1712@users.noreply.github.com>
2025-07-11 18:08:05 +0000

    Añadido color extra

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { Container } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from "react";
 
 
 export const App = () => {
-  
+
   const [colorSeleccionado, setColorSeleccionado] = useState('');
+  const [activeMorado, setActiveMorado] = useState(false);
 
   const claseRojo = "rojo" + (colorSeleccionado === "rojo" ? " selected" : "");
   const claseAmarillo = "amarillo" + (colorSeleccionado === "amarillo" ? " selected" : "");
   const claseVerde = "verde" + (colorSeleccionado === "verde" ? " selected" : "");
+  const claseMorado = "morado" + (colorSeleccionado === "morado" ? " selected" : "");
 
   return (
     <>
@@ -19,7 +21,12 @@ export const App = () => {
           <div className={claseRojo} onClick={() => setColorSeleccionado("rojo")}></div>
           <div className={claseAmarillo} onClick={() => setColorSeleccionado("amarillo")}></div>
           <div className={claseVerde} onClick={() => setColorSeleccionado("verde")}></div>
+          {activeMorado && (<div className={claseMorado} onClick={() => setColorSeleccionado("morado")}></div>)}          
         </div>
+        <Button 
+        onClick={() => setActiveMorado(true)}
+        className={activeMorado ? "ocultar": ""}
+        >Añadir color</Button>
       </Container>
     </>
   );
