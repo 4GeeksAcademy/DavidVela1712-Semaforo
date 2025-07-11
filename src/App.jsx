33521c6e6e1b93523b40961234438c4d1e1e7096
DavidVela1712 <75803006+DavidVela1712@users.noreply.github.com>
2025-07-11 17:31:47 +0000

    110725-Fin-Proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import { Container } from "react-bootstrap";
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { useState } from "react";
 
 
 export const App = () => {
+  
+  const [colorSeleccionado, setColorSeleccionado] = useState('');
+
+  const claseRojo = "rojo" + (colorSeleccionado === "rojo" ? " selected" : "");
+  const claseAmarillo = "amarillo" + (colorSeleccionado === "amarillo" ? " selected" : "");
+  const claseVerde = "verde" + (colorSeleccionado === "verde" ? " selected" : "");
+
   return (
     <>
       <Container fluid className="semaforo-contenedor">
         <div className="semaforo-caja">
-          <div className="rojo"></div>
-          <div className="amarillo selected"></div>
-          <div className="verde"></div>
+          <div className={claseRojo} onClick={() => setColorSeleccionado("rojo")}></div>
+          <div className={claseAmarillo} onClick={() => setColorSeleccionado("amarillo")}></div>
+          <div className={claseVerde} onClick={() => setColorSeleccionado("verde")}></div>
         </div>
       </Container>
     </>
